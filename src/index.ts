07a637acb3723288a7f97ac1902bf778b7a19dbc
Jing <mqjing@gmail.com>
2020-10-13 13:13:09 +0800

    [refact] update the launch.json and record the not-yet

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,41 @@
 import https = require('https');
-import {JSDOM} from'jsdom';
 import cheerio from 'cheerio';
 import fs = require('fs');
+var htmlparser = require('htmlparser2');
 
 const FILE_PEOPLE_NOT_PRESENT:string = "./data_people_not_present.csv"
 const HOSTNAME:string = 'npm.cpami.gov.tw';
 const Path:string = '/news_2.aspx?search=yes&org=c951cdcd-b75a-46b9-8002-8ef952ec95fd&Con=DESC&pageset=5000';
+const { WritableStream } = require("htmlparser2/lib/WritableStream");
+
+// ======= Chunk mode ===
+// function getWebDataStream(){
+//   // Step 1: Create a parser and matching the tag
+//   const parser = new WritableStream({
+//       onopentag(name:any, attribs:any) {
+//           if (name === "table") {
+//               console.log("table start");
+//           }
+//       },
+//       ontext(text:any) {
+//           console.log("-->", text);
+//       },
+//       onclosetag(tagname:any) {
+//         if (tagname === "table") {
+//           console.log("table end");
+//         }
+//       },
+//   });
+  
+//   // Step 2: Connect to the site and pipe the stream to the parser
+//   https.get("https://" + HOSTNAME+Path, (res:any) => res.pipe(parser).on("finish", () => console.log("done")));
+
+// }
+
+// function main(){
+//   getWebDataStream();
+// }
+// main();
 
 function getData(){
   return new Promise<Buffer>( (resolve, reject) => {
